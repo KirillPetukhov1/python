be1c8b6da1ac854874d32932daace50842438110
KirillPetuhov <kpetuhov3242@gmail.com>
2025-09-13 17:09:59 +0300

    lab 1 checking is list

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,6 +1,8 @@
 from typing import Optional, List
 
 def find_sum(nums: List[int], target: int) -> Optional[List[int]]:
+    if (type(nums) != list):
+        return None
     for i in range(len(nums) - 1):
         for j in range(i + 1, len(nums)):
             try:
