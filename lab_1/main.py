3d9bd5a69bf971b8453651d1b2c48e848a78d363
KirillPetuhov <kpetuhov3242@gmail.com>
2025-09-26 13:58:46 +0300

    some changes in main

--- lab_1/main.py
+++ lab_1/main.py
@@ -3,12 +3,12 @@ from typing import Optional, List
 
 def find_sum(nums: List[int], target: int) -> Optional[List[int]]:
     if (type(nums) != list):
-        return None
+        return
     for i in range(len(nums) - 1):
         for j in range(i + 1, len(nums)):
             try:
                 if int(nums[i]) + int(nums[j]) == int(target):
                     return [i, j]
             except (ValueError, TypeError):
-                return None
-    return None
+                return
+    return
